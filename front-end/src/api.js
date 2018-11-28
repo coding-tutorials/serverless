7bdcc66df46899d0057fa743085ee31a8f953c73
giseleml <giselemaraleo@gmail.com>
2018-11-28 19:44:06 -0200

    #9 Semaphore Animation and Pusher Messages

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -1,10 +1,13 @@
 const axios = require('axios')
 const uuidv1 = require('uuid/v1')
 
-const url = `https://2taws86syf.execute-api.us-west-2.amazonaws.com/prod/pictures/${uuidv1()}`
+const url = `https://2taws86syf.execute-api.us-west-2.amazonaws.com/prod`
 
-const getPictures = () => axios.get(url)
+const getPictures = (id) => axios.get(`${url}/pictures/${id}`)
+
+const getToken = () => axios.get(`${url}/token`)
 
 module.exports = {
-  getPictures
+  getPictures,
+  getToken
 }
