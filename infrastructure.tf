e45f45f48ba513cd4078c2db88122468040de043
Wagner Leonardi <leonardiwagner@gmail.com>
2018-11-28 15:04:07 -0200

    add cors to gateway api

--- infrastructure.tf
+++ infrastructure.tf
@@ -78,7 +78,8 @@ resource "aws_lambda_function" "example-lambda-middleware" {
 
   environment {
     variables = {
-      NODE_ENV = "production"
+      NODE_ENV = "production",
+      TR_VAR_aws_rds_password = "${var.aws_rds_password}"
     }
   }
 }
@@ -110,13 +111,33 @@ resource "aws_api_gateway_integration" "lambda_root" {
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.example-lambda-middleware.invoke_arn}"
 }
-/*
+
+
 resource "aws_api_gateway_method_response" "200" {
   rest_api_id = "${aws_api_gateway_rest_api.example-api-gateway.id}"
   resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
   http_method = "${aws_api_gateway_method.proxy_root.http_method}"
   status_code = "200"
-}*/
+  response_parameters {
+      "method.response.header.Access-Control-Allow-Headers" = true,
+      "method.response.header.Access-Control-Allow-Methods" = true,
+      "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+    rest_api_id = "${aws_api_gateway_rest_api.example-api-gateway.id}"
+    resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
+    http_method = "${aws_api_gateway_method.proxy_root.http_method}"
+    status_code   = "${aws_api_gateway_method_response.200.status_code}"
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
+    depends_on = ["aws_api_gateway_method_response.200"]
+}
+
 
 # resource "aws_api_gateway_method" "example-app-method1" {
 #   rest_api_id   = "${aws_api_gateway_rest_api.example-api-gateway.id}"
